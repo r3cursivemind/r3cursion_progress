50df029c48b39d54d64a1da6d0291f479a57a08d
insaf-arch <insaf.globalvpn@gmail.com>
2025-03-01 05:04:39 +0300

    quick sort and recursion1.c edit

---
+++ algorithms/quickSort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void quicksortMiddle(int array[], int low, int high) {
+  if (low < high) {
+      int pivot = array[(low + high) / 2];
+      int i = low;
+      int j = high;
+      int temp;
+
+      while (i <= j) {
+          while (array[i] < pivot) i++; 
+          while (array[j] > pivot) j--; 
+
+          if (i <= j) {
+              temp = array[i];        
+              array[i] = array[j];
+              array[j] = temp;
+              i++;
+              j--;
+          }
+      }
+
+      if (low < j) quicksortMiddle(array, low, j);
+      if (i < high) quicksortMiddle(array, i, high);
+  }
+}
+
+void printArray(int array[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+      printf("%d ", array[i]);
+  }
+  printf("\n");
+}
+
+int main()
+{
+  int array[] = {34, 62, 32, 23, 5, 7};
+  int n = sizeof(array) / sizeof(array[0]);
+
+  printf("Original Array: \n");
+  printArray(array, n);
+
+  quicksortMiddle(array, 0, n - 1);
+  printf("Sorted with Middle Element as Pivot: \n");
+  printArray(array, n);
+
+  return 0;
+}
--- algorithms/recursion1.c
+++ algorithms/recursion1.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int sum(int array[], int n) {
-  if (n <= 0) 
-      return 0;
-  return (sum(array, n - 1) + array[n - 1]);
+int sum(int *array, int n) {
+  int total = 0;
+  for (int i = 0; i < n; i++) {
+    total += array[i];
+  }
+  return total;
 }
 
 int main()
 {
   int array[] = {1, 2, 3, 4, 5};
   int n = sizeof(array) / sizeof(array[0]);
-
   printf("%d\n", sum(array, n));
   
 	return 0;
