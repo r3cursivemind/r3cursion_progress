d121a6dca26dc92b2b7b1e48dd955c6ffaaa473f
insaf-arch <insaf.globalvpn@gmail.com>
2025-03-16 03:10:36 +0300

    added description

--- stepik/DSA/2.3(1).py
+++ stepik/DSA/2.3(1).py
@@ -1,3 +1,8 @@
+#You have decided to write the first IT-project for further sale to Giggle company, which draws pseudo-graphic Christmas trees with NN beauty in the following way (in the example, Christmas trees with beauty from 0 to 5 are drawn):
+
+#When this project went into production, you made an edit - the program should additionally calculate the area of a Christmas tree with beauty NN. Realize the algorithm for calculating the area of a Christmas tree with a given beauty of order O(1)O(1).
+
+
 n = int(input())
 trunk = 0
 result = 0
