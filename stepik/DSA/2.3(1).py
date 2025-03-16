8fa2f79ce46963c707d42d5d3a9f421e9a059917
insaf-arch <insaf.globalvpn@gmail.com>
2025-03-16 03:06:06 +0300

    started a course on stepik

---
+++ stepik/DSA/2.3(1).py
@@ -0,0 +1,52 @@
+n = int(input())
+trunk = 0
+result = 0
+
+#if n >= 0:
+for i in range(0, n + 1):
+    _pow = i * 2 
+    branches += _pow
+    trunk = (n * 2 + 1)
+    result = trunk + branches
+print(result)
+#else:
+#    print("Wrong number") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
